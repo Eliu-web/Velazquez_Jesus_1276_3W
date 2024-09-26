cde618b31febd9f424ae8f5c39a528f84be581aa
Eliu-web <a.23308051281276@cbtis128.edu.mx>
2024-09-26 14:45:02 -0600

    Add files via upload

---
+++ Velazquez_Jesus_1276_3W.py
@@ -0,0 +1,13 @@
+print (" ") #Salto de linea
+print ("velazquez mares jesus eliu") #Muestra en pantalla el nombre del creador del programa
+print (" ")#Salto de linea
+
+a1 = ("velazquez")  #Definimos variable y le asignamos un valor tipo str
+a2 = ("mares")#Definimos variable y le asignamos un valor tipo str
+n1 = ("jesus")#Definimos variable y le asignamos un valor tipo str
+n2 = ("eliu")#Definimos variable y le asignamos un valor tipo str
+t = (a1 + " " + a2 + " " + n1 + " " + n2) #unimos todas las variables tipo str en una sola
+
+
+print (t.upper()) #Aggarra la cadena de str y la pone en mayusculas e imprime en panatlla
+
