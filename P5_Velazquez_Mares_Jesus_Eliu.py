ccc755fea726504c1d6fb0649b3c86987e4a5ab1
Eliu-web <a.23308051281276@cbtis128.edu.mx>
2024-09-26 15:52:39 -0600

    Add files via upload

---
+++ P5_Velazquez_Mares_Jesus_Eliu.py
@@ -0,0 +1,19 @@
+print (" ") #Salto de linea
+print ("velazquez mares jesus eliu") #Muestra en pantalla el nombre del creador del programa
+print (" ")#Salto de linea
+
+a = (int(input("Ingresa un valor entero: "))) #Marca una estrada 
+b = (int(input("Ingresa un segundo valor entero: ")))#Marca una estrada 
+
+#Estructura de decicion
+if a < b: #si a es menor que b 
+    print (a, "es el menor")#Entonces imprime en pantalla a es el menor
+    print (" ")#Salto de linea
+elif b < a :#si b es menor que a 
+    print (b, "es el menor")#Entonces imprime en pantalla b es el menor
+    print (" ")#Salto de linea
+
+suma = a + b #suma la variables a y b
+
+print("La suma de los valores es",a,"y",b,"es de: ",suma)#Mustra en pantalla el resultado de la suma
+
