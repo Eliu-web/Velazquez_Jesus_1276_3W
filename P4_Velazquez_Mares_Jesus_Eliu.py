74dfc288a152c025fc6480d4a72cff37e6b982d4
Eliu-web <a.23308051281276@cbtis128.edu.mx>
2024-09-26 15:21:02 -0600

    Add files via upload

---
+++ P4_Velazquez_Mares_Jesus_Eliu.py
@@ -0,0 +1,24 @@
+print (" ") #Salto de linea
+print ("velazquez mares jesus eliu") #Muestra en pantalla el nombre del creador del programa
+print (" ")#Salto de linea
+
+a = (int(input("Ingresa un valor entero: "))) #Marca una estrada de datos de tipo entero
+b = (int(input("Ingresa un segundo valor entero: ")))#Marca una estrada de datos de tipo entero
+c = (int(input("Ingresa un tercer valor entero: ")))#Marca una estrada de datos de tipo entero
+#Estructura de decicion
+if a > b and a > c: #si a es mayor que b y a es mayor que c 
+    print (a, "es el mayor")#Entonces imprime en pantalla a es el mayor
+elif b > a and b > c:#si b es mayor que a y b es mayor que c 
+    print (b, "es el mayor")#Entonces imprime en pantalla b es el mayor
+elif c > b and c > a:#si c es mayor que a y c es mayor que b 
+    print (c, "es el mayor")#Entonces imprime en pantalla c es el mayor
+elif a == b and b == c and c == a:#si a es igual que b y b es igual que c y a es igual que c 
+    print ("ingrsa valores no congruentes")#Entonces imprime en pantalla ingrsa valores no congruentes
+
+#Estructura de decicion
+if a < b and a < c:#si a es menor que b y a es menor que c 
+    print (a, "es el menor")#Entonces imprime en pantalla a es el menor
+elif b < a and b < c:#si b es menor que a y b es menor que c 
+    print (b, "es el menor")#Entonces imprime en pantalla b es el menor
+elif c < b and c < b:#si c es menor que a y c es menor que a
+    print (c, "es el menor")#Entonces imprime en pantalla c es el menor1
