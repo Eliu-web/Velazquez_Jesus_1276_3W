db5ceed8a735dbf2490d172d04cf0b3a371bf973
Eliu-web <a.23308051281276@cbtis128.edu.mx>
2024-09-27 10:39:22 -0600

    Add files via upload

---
+++ P6_Velazquez_Mares_Jesus-eliu.py
@@ -0,0 +1,10 @@
+print (" ") #Salto de linea
+print ("velazquez mares jesus eliu") #Muestra en pantalla el nombre del creador del programa
+print (" ")#Salto de linea
+
+b = float(input("ingrese la base del rectangulo: "))#Marca una estrada de datos
+h = float(input("ingresa la altura del rectangulo: 5"))#Marca una estrada de datos
+
+p = (b * 2) + (h * 2) #multiplica por dos las variable b y h y las suma
+
+print ("El perimetro del rectangulo es de: " , p)#Imprime en pantalla el perimetro
